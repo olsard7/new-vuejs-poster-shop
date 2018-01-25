623cc0666854911d76a503d9c64d170428471baa
Oleg <reg.barsuk@gmail.com>
2018-01-25 21:02:10 +0300

    add @click for product cards

--- public/script.js
+++ public/script.js
@@ -1,6 +1,17 @@
 new Vue ({
     el: '#app',
     data: {
-        total: 0
+        total: 0,
+        items: [
+            { title: 'Item1 ' },
+            { title: 'Item 2' },
+            { title: 'Item 3 '}
+        ]
+    },
+    methods: {
+        addItem: function(index) {
+            console.log(index);
+            this.total +=9.99;
+        }
     }
 })
