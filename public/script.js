1be8f5fa32200d3e0a9f7dfe86c11a966711a327
Oleg <reg.barsuk@gmail.com>
2018-02-07 19:42:00 +0300

    lesson 31. Add v-model for search input

--- public/script.js
+++ public/script.js
@@ -9,11 +9,12 @@ new Vue({
             { id: 2, title: 'Item 2' },
             { id: 3, title: 'Item 3 ' }
         ],
-        cart: []
+        cart: [],
+        search: ''
     },
     methods: {
         onSubmit: function() {
-            console.log('Submitted');
+            console.log(this.search);
         },
         addItem: function (index) {
             this.total += PRICE;
